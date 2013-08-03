2cb8e5fff55c56aa3449fefc80fd46ac48a7623a
Ben Lin <synchrokun@gmail.com>
2013-08-03 13:31:48 -0700

    Initial Commit

---
+++ iToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iToDo
+//
+//  Created by Ben Lin on 8/3/13.
+//  Copyright (c) 2013 Ben Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iToDo
+//
+//  Created by Ben Lin on 8/3/13.
+//  Copyright (c) 2013 Ben Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
